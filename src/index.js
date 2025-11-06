09bddb368acb77497c076cee68c3898609d4d060
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-11-06 13:31:25 -0600

    in HTML  added IDs so we can select all the cities and an individual city so we can use in JS, in JS we added a function that gets called everytime someone selects an option, and replace the HTML with the data JS pulls using moment API with live times over specific timezones

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 function updateTime() {
   let kyotoElement = document.querySelector("#kyoto");
-  let kyotoDateElement = kyotoElement.querySelector(".date");
-  let kyotoTimeElement = kyotoElement.querySelector(".time");
-  let currentKyotoTime = moment().tz("Asia/Tokyo");
-  kyotoDateElement.innerHTML = currentKyotoTime.format("MMMM Do YYYY");
-  kyotoTimeElement.innerHTML = currentKyotoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (kyotoElement) {
+    let kyotoDateElement = kyotoElement.querySelector(".date");
+    let kyotoTimeElement = kyotoElement.querySelector(".time");
+    let currentKyotoTime = moment().tz("Asia/Tokyo");
+    kyotoDateElement.innerHTML = currentKyotoTime.format("MMMM Do YYYY");
+    kyotoTimeElement.innerHTML = currentKyotoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let denmarkElement = document.querySelector("#denmark");
-  let denmarkDateElement = denmarkElement.querySelector(".date");
-  let denmarkTimeElement = denmarkElement.querySelector(".time");
-  let currentDenmarkTime = moment().tz("Europe/Copenhagen");
-  denmarkDateElement.innerHTML = currentDenmarkTime.format("MMMM Do YYYY");
-  denmarkTimeElement.innerHTML = currentDenmarkTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (denmarkElement) {
+    let denmarkDateElement = denmarkElement.querySelector(".date");
+    let denmarkTimeElement = denmarkElement.querySelector(".time");
+    let currentDenmarkTime = moment().tz("Europe/Copenhagen");
+    denmarkDateElement.innerHTML = currentDenmarkTime.format("MMMM Do YYYY");
+    denmarkTimeElement.innerHTML = currentDenmarkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = event.target.options[event.target.selectedIndex].text;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        )}</div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
