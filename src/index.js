cee6c23164177db53f01b6db373a55ef5f14d1c3
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-11-06 12:41:22 -0600

    added first round of javascript, can very much be improved

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  let kyotoElement = document.querySelector("#kyoto");
+  let kyotoDateElement = kyotoElement.querySelector(".date");
+  let kyotoTimeElement = kyotoElement.querySelector(".time");
+  let currentKyotoTime = moment().tz("Asia/Tokyo");
+  kyotoDateElement.innerHTML = currentKyotoTime.format("MMMM Do YYYY");
+  kyotoTimeElement.innerHTML = currentKyotoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  let denmarkElement = document.querySelector("#denmark");
+  let denmarkDateElement = denmarkElement.querySelector(".date");
+  let denmarkTimeElement = denmarkElement.querySelector(".time");
+  let currentDenmarkTime = moment().tz("Europe/Copenhagen");
+  denmarkDateElement.innerHTML = currentDenmarkTime.format("MMMM Do YYYY");
+  denmarkTimeElement.innerHTML = currentDenmarkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
